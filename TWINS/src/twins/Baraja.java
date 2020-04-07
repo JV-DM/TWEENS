a2925caf402e0d39d23659fd06fe46e61e20cb0f
jacusiUPV <62893649+jacusiUPV@users.noreply.github.com>
2020-04-07 12:35:55 +0200

    Añadida variable tamaño a Baraja

--- TWINS/src/twins/Baraja.java
+++ TWINS/src/twins/Baraja.java
@@ -15,23 +15,32 @@ public class Baraja {
     private List<Carta> cartas;
     private String nombre;
     private CaraPosterior caraPosterior;
+    private int tamaño;
     
-    public Baraja(List<Carta> cartas, String nombre, CaraPosterior caraPosterior) {
+    public Baraja(List<Carta> cartas, String nombre, CaraPosterior caraPosterior,
+            int tamaño) {
         this.cartas = cartas;
         this.nombre = nombre;
         this.caraPosterior = caraPosterior;
+        this.tamaño = tamaño;
     }
     
-    public List<Carta> GetCartas() {return this.cartas;}
-    public String GetNombre() {return this.nombre;}
+    public List<Carta> GetCartas(){return this.cartas;}
+    public String GetNombre(){return this.nombre;}
     public CaraPosterior GetCaraPosterior(){return this.caraPosterior;}
+    public int GetTamaño(){return this.tamaño;}
     
-    public void SetCartas(List<Carta> nuevaCartas) {this.cartas = nuevaCartas;}
-    public void SetNombre(String nuevoNombre) {this.nombre = nuevoNombre;}
+    public void SetCartas(List<Carta> nuevaCartas){this.cartas = nuevaCartas;}
+    public void SetNombre(String nuevoNombre){this.nombre = nuevoNombre;}
     public void SetCaraPosterior(CaraPosterior nuevaCaraPosterior){
         this.caraPosterior = nuevaCaraPosterior;
     }
+    public void SetTamaño(int nuevoTamaño){this.tamaño = nuevoTamaño;}
     
-    public void AñadirCarta(Carta carta){cartas.add(carta);}
-    public void EliminarCarta(Carta carta){cartas.remove(carta);}
+    public void AñadirCarta(Carta carta){
+        cartas.add(carta); tamaño++;
+    }
+    public void EliminarCarta(Carta carta){
+        cartas.remove(carta); tamaño--;
+    }
 }
