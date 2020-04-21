a64c7ed38509f7e21af3a90518c39286e9866044
jacusiUPV <62893649+jacusiUPV@users.noreply.github.com>
2020-04-21 20:39:31 +0200

    Pantalla final de partida

--- TWINS/src/data_type/Baraja.java
+++ TWINS/src/data_type/Baraja.java
@@ -12,6 +12,10 @@ import java.util.List;
  *
  * @author Javier
  */
+
+/**
+ * Constructor de la baraja
+ */
 public class Baraja {
     private List<Carta> cartas;
     private String nombre;
@@ -27,40 +31,83 @@ public class Baraja {
         this.caraPosterior = caraPosterior;
         this.tamaño = tamaño;
     }
-    
-    public List<Carta> GetCartas(){return this.cartas;}
-    public String GetNombre(){return this.nombre;}
-    public CaraPosterior GetCaraPosterior(){return this.caraPosterior;}
-    public int GetTamaño(){return this.tamaño;}
-    
+
+    /**
+     * Devuelve la lista de cartas
+     * @return lista de cartas
+     */
+    public List<Carta> getCartas(){return this.cartas;}
+
+    /**
+     * Devuelve el nombre de la baraja
+     * @return nombre de la baraja
+     */
+    public String getNombre(){return this.nombre;}
+
+    /**
+     * Devuelve la cara posterior de las cartas
+      * @return cara posterior de las cartas de la baraja
+     */
+    public CaraPosterior getCaraPosterior(){return this.caraPosterior;}
+
+    /**
+     * Devuelve el tamaño de la baraja(numero de cartas)
+     * @return
+     */
+    public int getTamaño(){return this.tamaño;}
+
+    /**
+     * Cambia la lista de cartas de la baraja
+     * @param nuevaCartas nueva lista de cartas
+     */
     public void SetCartas(List<Carta> nuevaCartas){this.cartas = nuevaCartas;}
-    public void SetNombre(String nuevoNombre){this.nombre = nuevoNombre;}
-    public void SetCaraPosterior(CaraPosterior nuevaCaraPosterior){
+
+    /**
+     * Cambia el nombre de la baraja
+     * @param nuevoNombre nuevo nombre de la baraja
+     */
+    public void setNombre(String nuevoNombre){this.nombre = nuevoNombre;}
+
+    /**
+     * Cambia la cara posterior de la baraja
+     * @param nuevaCaraPosterior
+     */
+    public void setCaraPosterior(CaraPosterior nuevaCaraPosterior){
         this.caraPosterior = nuevaCaraPosterior;
     }
-    public void SetTamaño(int nuevoTamaño){this.tamaño = nuevoTamaño;}
-    
-    public boolean añadirCarta(Carta carta){
-        if(carta == null) return false;
+
+    /**
+     * cambia el tamaño de la baraja
+     * @param nuevoTamaño
+     */
+    public void setTamaño(int nuevoTamaño){this.tamaño = nuevoTamaño;}
+
+    /**
+     * Añade una carta a la baraja
+     * @param carta
+     */
+    public void añadirCarta(Carta carta){
+        if(carta == null) throw new NullPointerException();
         if(cartas == null){
             cartas = new ArrayList<>();
             cartas.add(carta);
             cartas.add(new Carta(carta.getImagen(),carta.getNombre(), carta.getId()));
             tamaño += 2;
-            return true;
         }
-        else if(!ExisteLaCarta(carta)) {
+        else if(!existeLaCarta(carta)) {
             cartas.add(carta);
             cartas.add(new Carta(carta.getImagen(),carta.getNombre(), carta.getId()));
             tamaño += 2;
-            return true;
         }
-        return false;
     }
-    
-    public boolean EliminarCarta(Carta carta){
+
+    /**
+     * Elimina una carta de la baraja
+     * @param carta carta a eliminar
+     */
+    public boolean eliminarCarta(Carta carta){
         if(carta == null) return false;
-        else if(!ExisteLaCarta(carta)){
+        else if(!existeLaCarta(carta)){
             cartas.remove(carta);
             cartas.remove(carta);
             tamaño-= 2;
@@ -68,12 +115,20 @@ public class Baraja {
         }
         return false;
     }
-    
-    public boolean ExisteLaCarta(Carta cartaNueva){
+
+    /**
+     * @param cartaNueva
+     * @return true if the card exist
+     */
+    public boolean existeLaCarta(Carta cartaNueva){
         if(cartas == null) return false;
         return cartas.contains(cartaNueva);
     }
-    
+
+    /**
+     * @param baraja
+     * @return true si 2 barajas son iguales
+     */
     public boolean EqualsTo(Baraja baraja){
         return this.nombre.equals(baraja.nombre);
     }
--- TWINS/src/view/mainViewController.java
+++ TWINS/src/view/mainViewController.java
@@ -1,11 +1,9 @@
 package view;
 
-import data_type.Baraja;
 import data_type.Carta;
 import data_type.GestorBarajas;
 import data_type.Partida;
 import javafx.application.Platform;
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -16,10 +14,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 
 import java.util.*;
-
+import java.util.concurrent.TimeUnit;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 
 
 public class mainViewController {
@@ -52,15 +51,25 @@ public class mainViewController {
 
     private Map<Carta, ImageView> cardImageViewMap = new HashMap<>();
 
-    private void gridCreation(List<Carta> cardList, ReadOnlyDoubleProperty height, ReadOnlyDoubleProperty width){
+    private long time = ONE_MINUTE;
+
+    /**
+     * Crea un gridPane con las cartas (mostrando la parte de atrás de la carta) con su posición random
+     * (los parametros height y width en principio se usarán en los próximos sprints, creo)
+     * @param cards lista de cartas de la baraja
+     * @param height numero de columnas
+     * @param width número de filas
+     **/
+    private void gridCreation(List<Carta> cards, ReadOnlyDoubleProperty height, ReadOnlyDoubleProperty width){
         int cardNumber = 0;
-
         playGridPane = new GridPane();
+        // aesthetics
         playGridPane.setPadding(new Insets(10));
-
+        List<Carta> cardList = new ArrayList<>(cards);
+        Collections.shuffle(cardList);
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 5; j++){
-                Image image = partida.getBaraja().GetCaraPosterior().GetImagen();
+                Image image = partida.getBaraja().getCaraPosterior().getImagen();
                 ImageView imageView = new ImageView(image);
                 imageView.setPreserveRatio(false);
                 imageView.fitWidthProperty().bind(width.divide(9));
@@ -71,6 +80,7 @@ public class mainViewController {
                 cardNumber += 1;
             }
         }
+        //more aesthetics
         playGridPane.setAlignment(Pos.CENTER);
         mainBorderPane.setCenter(playGridPane);
         mainBorderPane.setBackground(new Background(new BackgroundImage(
@@ -81,6 +91,12 @@ public class mainViewController {
                 new BackgroundSize(100, 100, true,true, false, true))));
     }
 
+    /**
+     * Manejador de eventos para TODAS las cartas
+     * @param carta carta que se supone que hay debajo de cada imagen de cara posterior
+     * @param imageView objeto de la imagen que se ha pulsado
+     * @return
+     */
     private EventHandler clickOnCardEventHandler(Carta carta, ImageView imageView) {
         return event ->{
             partida.pickCard(carta);
@@ -99,29 +115,42 @@ public class mainViewController {
                     }
                     for (Carta cardFromMap : cardImageViewMap.keySet())
                         if(!cardFromMap.isFound())
-                            cardImageViewMap.get(cardFromMap).setImage(partida.getBaraja().GetCaraPosterior().GetImagen());
+                            cardImageViewMap.get(cardFromMap).setImage(partida.getBaraja().getCaraPosterior().getImagen());
                     playGridPane.setDisable(false);
                 }
             }).start();
         };
     }
 
+    /**
+     * Método que actualiza el contador de tiempo a cada segundo
+     */
     private void updateTimer(){
         partida.getTimer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 Platform.runLater(() -> {
-                    long seconds = partida.getTimeLasted().toMillis();
-
-                    timeLabel.setText(formatTime(seconds));
-
-                    if(!partida.isRunning())
+                    time -= ONE_SECOND;
+                    if(time >= 0)
+                        timeLabel.setText(formatTime(time));
+                    if(time <= 0) {
                         partida.stopTimer();
+                        pantallaFinPartida();
+                    }
+                    if(partida.isFinished()){
+                        partida.stopTimer();
+                        pantallaFinPartida();
+                    }
                 });
             }
         },0,1000);
     }
 
+    /**
+     * Método para poner formato al label del tiempo
+     * @param duration tiempo que ha pasado desde que ha empezado la partida
+     * @return
+     */
     public static String formatTime(long duration){
         String res;
         duration /= ONE_SECOND;
@@ -138,15 +167,28 @@ public class mainViewController {
         }
         return res;
     }
+    
+    public void pantallaFinPartida(){
+        playGridPane.setVisible(false);
+        Label finalDePartida = new Label();
+        finalDePartida.setText("     PUNTUACIÓN \n               " 
+                + "0 \n"
+                + "TIEMPO DE PARTIDA \n          " 
+                + formatTime(60 - partida.getTimeLasted().getSeconds()));
+        finalDePartida.setTextFill(Paint.valueOf("white"));        
+        finalDePartida.setFont(Font.font("anton"));
+        finalDePartida.setFont(Font.font(50));
+        mainBorderPane.setCenter(finalDePartida);
+    }
 
     @FXML
     private void initialize(){
         //Esta linea se deberá eliminar posteriormente
         GestorBarajas gestor = new GestorBarajas();
-        gestor.CargarBarajaPorDefecto();
-        Partida p = new Partida(gestor.GetBarajaPorDefecto(),new Image("imagenes/ImagenesBackground/fondo-verde.jpg"));
+        gestor.cargarBarajaPorDefecto();
+        Partida p = new Partida(gestor.getBarajaPorDefecto(),new Image("imagenes/ImagenesBackground/fondo-verde.jpg"));
         partida = p;
-        List<Carta> cardList = p.getBaraja().GetCartas();
+        List<Carta> cardList = p.getBaraja().getCartas();
         partida.startGame();
         updateTimer();
         gridCreation(cardList, mainBorderPane.heightProperty(), mainBorderPane.widthProperty());
